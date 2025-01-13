e0bb9f60cf9f3e9fe7413cc8a2f7ffc479137be2
Vo Quang Chien <2giosangmitom@proton.me>
2025-01-13 15:44:32 +0700

    js: add merge two sorted lists

---
+++ JS/src/merge_two_sorted_lists.js
@@ -0,0 +1,46 @@
+import { ListNode } from "../lib/linked_list.js";
+
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+function mergeTwoLists(list1, list2) {
+  const res = new ListNode();
+  let head = res;
+
+  while (list1 && list2) {
+    const num1 = list1.val;
+    const num2 = list2.val;
+
+    if (num1 <= num2) {
+      const newNode = new ListNode(num1);
+      head.next = newNode;
+      head = head.next;
+      list1 = list1.next;
+    } else {
+      const newNode = new ListNode(num2);
+      head.next = newNode;
+      head = head.next;
+      list2 = list2.next;
+    }
+  }
+
+  while (list1) {
+    const newNode = new ListNode(list1.val);
+    head.next = newNode;
+    head = head.next;
+    list1 = list1.next;
+  }
+
+  while (list2) {
+    const newNode = new ListNode(list2.val);
+    head.next = newNode;
+    head = head.next;
+    list2 = list2.next;
+  }
+
+  return res.next;
+}
+
+export { mergeTwoLists };
