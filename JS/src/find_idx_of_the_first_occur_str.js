7f56b7ac63bee52d03af64690596a6da51e7c827
Vo Quang Chien <2giosangmitom@proton.me>
2025-01-13 16:01:23 +0700

    js: add Find the Index of the First Occurrence in a String

---
+++ JS/src/find_idx_of_the_first_occur_str.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+function strStr(haystack, needle) {
+  const haystackLen = haystack.length;
+  const needleLen = needle.length;
+
+  if (needleLen > haystackLen) return -1;
+
+  for (let i = 0; i <= haystackLen - needleLen; i++) {
+    const s = haystack.slice(i, i + needleLen);
+    if (s === needle) {
+      return i;
+    }
+  }
+
+  return -1;
+}
+
+export { strStr };
