fcba157bfa01086e923d74e0f86287cab651905c
Vo Quang Chien <2giosangmitom@proton.me>
2025-01-13 19:32:11 +0700

    js: add multiply strings

---
+++ JS/src/multiply_strings.js
@@ -0,0 +1,79 @@
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+function multiply(num1, num2) {
+  if (num1 === "0" || num2 === "0") {
+    return "0";
+  }
+
+  const reversedNum1 = [...num1].reverse().join("");
+  const reversedNum2 = [...num2].reverse().join("");
+
+  const parts = [];
+  const l1 = reversedNum1.length;
+  const l2 = reversedNum2.length;
+
+  for (let i = 0; i < l2; i++) {
+    let rs = "0".repeat(i);
+    let carry = 0;
+
+    for (let j = 0; j < l1; j++) {
+      const mul = Number(reversedNum2[i]) * Number(reversedNum1[j]) + carry;
+      const digit = mul % 10;
+      carry = Math.floor(mul / 10);
+
+      rs += digit;
+    }
+
+    if (carry !== 0) {
+      rs += carry;
+    }
+
+    parts.push(rs);
+  }
+
+  let sum = "0".repeat(l1 + l2);
+
+  for (const part of parts) {
+    sum = add(sum, part);
+  }
+
+  sum = sum.replace(/0+$/, "");
+
+  return [...sum].reverse().join("");
+}
+
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+function add(a, b) {
+  const n1 = a.length;
+  const n2 = b.length;
+
+  let result = "";
+  let i = 0,
+    j = 0,
+    carry = 0;
+
+  while (i < n1 || j < n2 || carry !== 0) {
+    const digit1 = i < n1 ? Number(a[i]) : 0;
+    const digit2 = j < n2 ? Number(b[j]) : 0;
+
+    const sum = digit1 + digit2 + carry;
+
+    const digit = sum % 10;
+    carry = Math.floor(sum / 10);
+
+    result += digit;
+    i++;
+    j++;
+  }
+
+  return result;
+}
+
+export { multiply };
---
+++ JS/tests/multiply_string.test.js
@@ -0,0 +1,45 @@
+import assert from "node:assert";
+import { describe, it } from "node:test";
+import { multiply } from "../src/multiply_strings.js";
+
+describe("multiply strings", () => {
+  it("should return '6' for input '2' and '3'", () => {
+    const result = multiply("2", "3");
+    assert.strictEqual(result, "6");
+  });
+
+  it("should return '56088' for input '123' and '456'", () => {
+    const result = multiply("123", "456");
+    assert.strictEqual(result, "56088");
+  });
+
+  it("should return '0' for input '0' and '12345'", () => {
+    const result = multiply("0", "12345");
+    assert.strictEqual(result, "0");
+  });
+
+  it("should return '0' for input '12345' and '0'", () => {
+    const result = multiply("12345", "0");
+    assert.strictEqual(result, "0");
+  });
+
+  it("should return '1' for input '1' and '1'", () => {
+    const result = multiply("1", "1");
+    assert.strictEqual(result, "1");
+  });
+
+  it("should return '99980001' for input '9999' and '9999'", () => {
+    const result = multiply("9999", "9999");
+    assert.strictEqual(result, "99980001");
+  });
+
+  it("should return '987654321987654321' for input '987654321' and '1000000001'", () => {
+    const result = multiply("987654321", "1000000001");
+    assert.strictEqual(result, "987654321987654321");
+  });
+
+  it("should return '100000000000000000000' for input '10000000000' and '10000000000'", () => {
+    const result = multiply("10000000000", "10000000000");
+    assert.strictEqual(result, "100000000000000000000");
+  });
+});
