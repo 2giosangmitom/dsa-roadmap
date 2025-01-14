05b86d2fc2e41cc2feb6c9d99a229f855f201b71
Vo Quang Chien <2giosangmitom@proton.me>
2025-01-14 11:40:36 +0700

    cc: add two sum

---
+++ CC/src/two_sum.hpp
@@ -0,0 +1,24 @@
+#ifndef TWO_SUM_HPP
+#define TWO_SUM_HPP
+
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  vector<int> twoSum(vector<int> &nums, int target) {
+    unordered_map<int, int> hashMap;
+    for (size_t i = 0; i < nums.size(); i++) {
+      int remainder = target - nums[i];
+      if (hashMap.find(remainder) == nullptr) {
+        hashMap.insert({nums[i], i});
+      } else {
+        return vector<int>{hashMap[remainder], static_cast<int>(i)};
+      }
+    }
+    return vector<int>{};
+  }
+};
+
+#endif // !TWO_SUM_HPP
---
+++ CC/tests/two_sum_test.cc
@@ -0,0 +1,44 @@
+#include "../src/two_sum.hpp"
+#include <gtest/gtest.h>
+#include <vector>
+
+class TwoSumTest : public ::testing::Test {
+protected:
+  Solution solution;
+};
+
+TEST_F(TwoSumTest, BasicTest) {
+  vector<int> nums = {2, 7, 11, 15};
+  int target = 9;
+  vector<int> expected = {0, 1};
+  EXPECT_EQ(solution.twoSum(nums, target), expected);
+}
+
+TEST_F(TwoSumTest, NegativeNumbers) {
+  vector<int> nums = {-3, 4, 3, 90};
+  int target = 0;
+  vector<int> expected = {0, 2};
+  EXPECT_EQ(solution.twoSum(nums, target), expected);
+}
+
+TEST_F(TwoSumTest, NoSolution) {
+  vector<int> nums = {1, 2, 3};
+  int target = 10;
+  vector<int> expected = {};
+  EXPECT_EQ(solution.twoSum(nums, target), expected);
+}
+
+TEST_F(TwoSumTest, Duplicates) {
+  vector<int> nums = {3, 3};
+  int target = 6;
+  vector<int> expected = {0, 1};
+  EXPECT_EQ(solution.twoSum(nums, target), expected);
+}
+
+TEST_F(TwoSumTest, LargeInput) {
+  vector<int> nums(1000, 1);
+  nums[999] = 5;
+  int target = 6;
+  vector<int> expected = {0, 999};
+  EXPECT_EQ(solution.twoSum(nums, target), expected);
+}
