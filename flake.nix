dd566b8a0d0214cefc57bb04cb10a46b1c07a77e
Vo Quang Chien <2giosangmitom@proton.me>
2025-01-15 11:21:04 +0700

    dev: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "LeetCode solutions implemented in JS and C++";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    systems = ["x86_64-linux" "aarch64-linux"];
+
+    forAllSystems = f:
+      nixpkgs.lib.genAttrs systems (system: let
+        pkgs = import nixpkgs {inherit system;};
+      in
+        f pkgs system);
+  in {
+    devShells = forAllSystems (pkgs: system: {
+      default = pkgs.mkShellNoCC {
+        buildInputs = with pkgs; [
+          nodejs_23
+          clang
+          gtest
+        ];
+        shellHook = ''
+          export GTEST="${pkgs.gtest}"
+          export GTEST_DEV="${pkgs.gtest.dev}/include"
+        '';
+      };
+    });
+    formatter = forAllSystems (pkgs: system: pkgs.alejandra);
+  };
+}
