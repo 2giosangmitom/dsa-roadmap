541bb00fb56737653bced8b578e0f26e40e81c5a
Vo Quang Chien <2giosangmitom@proton.me>
2025-01-19 14:35:01 +0700

    js: add plus one

---
+++ tests/plus_one.test.js
@@ -0,0 +1,41 @@
+import { plusOne } from "../src/plus_one.js";
+import { describe, it } from "node:test";
+import assert from "node:assert";
+
+describe("plusOne", { concurrency: true, timeout: 5000 }, () => {
+  it("should return [1,2,4] for input [1,2,3]", () => {
+    const digits = [1, 2, 3];
+    const expected = [1, 2, 4];
+
+    const actual = plusOne(digits);
+
+    assert.deepStrictEqual(actual, expected);
+  });
+
+  it("should return [1,3,0] for input [1,2,9]", () => {
+    const digits = [1, 2, 9];
+    const expected = [1, 3, 0];
+
+    const actual = plusOne(digits);
+
+    assert.deepStrictEqual(actual, expected);
+  });
+
+  it("should return [9,9,9] for input [9,9,8]", () => {
+    const digits = [9, 9, 8];
+    const expected = [9, 9, 9];
+
+    const actual = plusOne(digits);
+
+    assert.deepStrictEqual(actual, expected);
+  });
+
+  it("should return [1,0,0,0] for input [9,9,9]", () => {
+    const digits = [9, 9, 9];
+    const expected = [1, 0, 0, 0];
+
+    const actual = plusOne(digits);
+
+    assert.deepStrictEqual(actual, expected);
+  });
+});
