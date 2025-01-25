20fc65b091daa445d3f1030f194dc6816bda15b3
Vo Quang Chien <2giosangmitom@proton.me>
2025-01-25 23:02:51 +0700

    refactor: format code for consistency in climbing stairs function and tests

--- src/climbing_stairs.js
+++ src/climbing_stairs.js
@@ -3,13 +3,13 @@
  * @return {number}
  */
 function climbStairs(n) {
-  const dp = [0, 1, 2];
+	const dp = [0, 1, 2];
 
-  for (let i = 3; i <= n; i++) {
-    dp[i] = dp[i - 1] + dp[i - 2];
-  }
+	for (let i = 3; i <= n; i++) {
+		dp[i] = dp[i - 1] + dp[i - 2];
+	}
 
-  return dp[n];
+	return dp[n];
 }
 
 export { climbStairs };
