fff2f4fe9bae0dac601339c9c7a485d5883b3d6e
Vo Quang Chien <2giosangmitom@proton.me>
2025-01-26 16:09:17 +0700

    feat: add removeDuplicates function with tests

---
+++ src/remove_duplicates_2.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function removeDuplicates(nums) {
+  let k = 0;
+
+  let count = 0;
+  let prev = nums[0];
+  for (let i = 0; i < nums.length; i++) {
+    if (prev === nums[i]) {
+      count++;
+    } else {
+      count = 1;
+      prev = nums[i];
+    }
+
+    if (count <= 2) {
+      nums[k++] = nums[i];
+    }
+  }
+
+  return k;
+}
+
+export { removeDuplicates };
