0b1516932e182c94c4eca29f614403938e5e6d05
Vo Quang Chien <2giosangmitom@proton.me>
2025-01-26 22:24:08 +0700

    update same tree

--- tests/same_tree.test.js
+++ tests/same_tree.test.js
@@ -1,62 +1,67 @@
 import assert from "node:assert";
 import { describe, it } from "node:test";
 import { TreeNode } from "../lib/binary_tree.js";
-import { isSameTree } from "../src/same_tree.js";
+import { isSameTree, isSameTree2 } from "../src/same_tree.js";
 
-describe("isSameTree", { concurrency: true, timeout: 5000 }, () => {
-	it("should return true for [1,2,3] [1,2,3]", () => {
-		const t1 = TreeNode.of(1, 2, 3);
-		const t2 = TreeNode.of(1, 2, 3);
+function runTest(name, callback) {
+	describe(name, { concurrency: true, timeout: 5000 }, () => {
+		it("should return true for [1,2,3] [1,2,3]", () => {
+			const t1 = TreeNode.of(1, 2, 3);
+			const t2 = TreeNode.of(1, 2, 3);
 
-		assert.strictEqual(isSameTree(t1, t2), true);
+			assert.strictEqual(callback(t1, t2), true);
+		});
+
+		it("should return false for [1,2] [1,null,2]", () => {
+			const t1 = TreeNode.of(1, 2);
+			const t2 = TreeNode.of(1, null, 2);
+
+			assert.strictEqual(callback(t1, t2), false);
+		});
+
+		it("should return false for [1,2,1] [1,1,2]", () => {
+			const t1 = TreeNode.of(1, 2, 1);
+			const t2 = TreeNode.of(1, 1, 2);
+
+			assert.strictEqual(callback(t1, t2), false);
+		});
+
+		it("should return true for empty trees", () => {
+			const t1 = null;
+			const t2 = null;
+
+			assert.strictEqual(callback(t1, t2), true);
+		});
+
+		it("should return false for one empty tree and one non-empty tree", () => {
+			const t1 = null;
+			const t2 = TreeNode.of(1);
+
+			assert.strictEqual(callback(t1, t2), false);
+		});
+
+		it("should return true for single-node trees with the same value", () => {
+			const t1 = TreeNode.of(1);
+			const t2 = TreeNode.of(1);
+
+			assert.strictEqual(callback(t1, t2), true);
+		});
+
+		it("should return false for single-node trees with different values", () => {
+			const t1 = TreeNode.of(1);
+			const t2 = TreeNode.of(2);
+
+			assert.strictEqual(callback(t1, t2), false);
+		});
+
+		it("should return false for trees with the same values but different structures", () => {
+			const t1 = TreeNode.of(1, 2, null, 3);
+			const t2 = TreeNode.of(1, null, 2, null, 3);
+
+			assert.strictEqual(callback(t1, t2), false);
+		});
 	});
+}
 
-	it("should return false for [1,2] [1,null,2]", () => {
-		const t1 = TreeNode.of(1, 2);
-		const t2 = TreeNode.of(1, null, 2);
-
-		assert.strictEqual(isSameTree(t1, t2), false);
-	});
-
-	it("should return false for [1,2,1] [1,1,2]", () => {
-		const t1 = TreeNode.of(1, 2, 1);
-		const t2 = TreeNode.of(1, 1, 2);
-
-		assert.strictEqual(isSameTree(t1, t2), false);
-	});
-
-	it("should return true for empty trees", () => {
-		const t1 = null;
-		const t2 = null;
-
-		assert.strictEqual(isSameTree(t1, t2), true);
-	});
-
-	it("should return false for one empty tree and one non-empty tree", () => {
-		const t1 = null;
-		const t2 = TreeNode.of(1);
-
-		assert.strictEqual(isSameTree(t1, t2), false);
-	});
-
-	it("should return true for single-node trees with the same value", () => {
-		const t1 = TreeNode.of(1);
-		const t2 = TreeNode.of(1);
-
-		assert.strictEqual(isSameTree(t1, t2), true);
-	});
-
-	it("should return false for single-node trees with different values", () => {
-		const t1 = TreeNode.of(1);
-		const t2 = TreeNode.of(2);
-
-		assert.strictEqual(isSameTree(t1, t2), false);
-	});
-
-	it("should return false for trees with the same values but different structures", () => {
-		const t1 = TreeNode.of(1, 2, null, 3);
-		const t2 = TreeNode.of(1, null, 2, null, 3);
-
-		assert.strictEqual(isSameTree(t1, t2), false);
-	});
-});
+runTest("isSameTree", isSameTree);
+runTest("isSameTree2", isSameTree2);
