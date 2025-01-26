202120aab018bd0fe47f9bb87f3ff4711c1ac728
Vo Quang Chien <2giosangmitom@proton.me>
2025-01-26 16:36:35 +0700

    add remove duplicates from sorted list

---
+++ src/remove_dups_from_sorted_list.js
@@ -0,0 +1,41 @@
+import { ListNode } from "../lib/linked_list.js";
+
+/**
+ * @param {ListNode} prevNode
+ * @param {ListNode} node
+ */
+function deleteNode(prevNode, node) {
+	if (prevNode) {
+		prevNode.next = node.next;
+	}
+	node = null;
+}
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+function deleteDuplicates(head) {
+	if (!head) {
+		return null;
+	}
+	let current = head;
+	let val = current.val;
+	let prev = head;
+
+	current = current.next;
+
+	while (current) {
+		if (current?.val === val) {
+			deleteNode(prev, current);
+		} else {
+			val = current.val;
+			prev = current;
+		}
+		current = current.next;
+	}
+
+	return head;
+}
+
+export { deleteDuplicates };
---
+++ tests/remove_dups_from_sorted_list.test.js
@@ -0,0 +1,69 @@
+import assert from "node:assert";
+import { describe, it } from "node:test";
+import { ListNode } from "../lib/linked_list.js";
+import { deleteDuplicates } from "../src/remove_dups_from_sorted_list.js";
+
+describe("deleteDuplicates", { concurrency: true, timeout: 5000 }, () => {
+	it("should return 1 -> 2 for input 1 -> 1 -> 2", () => {
+		const list = ListNode.of(1, 1, 2);
+		const expected = ListNode.of(1, 2);
+
+		const actual = deleteDuplicates(list);
+
+		assert.strictEqual(actual.toString(), expected.toString());
+	});
+
+	it("should return 1 -> 2 -> 3 for input 1 -> 1 -> 2 -> 3 -> 3", () => {
+		const list = ListNode.of(1, 1, 2, 3, 3);
+		const expected = ListNode.of(1, 2, 3);
+
+		const actual = deleteDuplicates(list);
+
+		assert.strictEqual(actual.toString(), expected.toString());
+	});
+
+	it("should return 1 for input 1 -> 1 -> 1", () => {
+		const list = ListNode.of(1, 1, 1);
+		const expected = ListNode.of(1);
+
+		const actual = deleteDuplicates(list);
+
+		assert.strictEqual(actual.toString(), expected.toString());
+	});
+
+	it("should return 1 -> 2 -> 3 -> 4 for input 1 -> 2 -> 3 -> 4", () => {
+		const list = ListNode.of(1, 2, 3, 4);
+		const expected = ListNode.of(1, 2, 3, 4);
+
+		const actual = deleteDuplicates(list);
+
+		assert.strictEqual(actual.toString(), expected.toString());
+	});
+
+	it("should return null for input null", () => {
+		const list = null;
+		const expected = null;
+
+		const actual = deleteDuplicates(list);
+
+		assert.strictEqual(actual, expected);
+	});
+
+	it("should return -3 -> -2 -> -1 -> 0 for input -3 -> -3 -> -2 -> -1 -> -1 -> 0", () => {
+		const list = ListNode.of(-3, -3, -2, -1, -1, 0);
+		const expected = ListNode.of(-3, -2, -1, 0);
+
+		const actual = deleteDuplicates(list);
+
+		assert.strictEqual(actual.toString(), expected.toString());
+	});
+
+	it("should return -2 -> -1 -> 0 -> 1 -> 2 for input -2 -> -1 -> 0 -> 1 -> 2 -> 2", () => {
+		const list = ListNode.of(-2, -1, 0, 1, 2, 2);
+		const expected = ListNode.of(-2, -1, 0, 1, 2);
+
+		const actual = deleteDuplicates(list);
+
+		assert.strictEqual(actual.toString(), expected.toString());
+	});
+});
