ac9d82e3e81d8dac7ca9407c823a05ad58be0e36
Vo Quang Chien <2giosangmitom@proton.me>
2025-01-29 20:57:53 +0700

    update iteration solution for myPow

--- src/pow.js
+++ src/pow.js
@@ -13,10 +13,33 @@ function myPow(x, n) {
 	ans = ans * ans;
 
 	if (pow % 2 === 1) {
-		ans = ans * x;
+		ans *= x;
 	}
 
 	return n < 0 ? 1 / ans : ans;
 }
 
-export { myPow };
+/**
+ * @param {number} x
+ * @param {number} n
+ * @returns {number}
+ */
+function myPow2(x, n) {
+	if (x === 0) return 0;
+	if (n === 0) return 1;
+
+	let p = Math.abs(n);
+	let res = 1;
+
+	while (p !== 0) {
+		if (p % 2 === 1) {
+			res *= x;
+		}
+		x *= x;
+		p = Math.trunc(p / 2);
+	}
+
+	return n < 0 ? 1 / res : res;
+}
+
+export { myPow, myPow2 };
