831d6c29d027f1414c438d67a8dadb15c8f64659
Vo Quang Chien <2giosangmitom@proton.me>
2025-02-08 13:50:36 +0700

    js: add 11

---
+++ JS/src/container_with_most_water.js
@@ -0,0 +1,46 @@
+/**
+ * Brute force approach (TLE)
+ * @param {number[]} height
+ * @return {number}
+ */
+function maxArea(height) {
+  let max = 0;
+
+  for (let i = 0; i < height.length - 1; i++) {
+    for (let j = i + 1; j < height.length; j++) {
+      const area = Math.min(height[i], height[j]) * (j - i);
+      max = Math.max(area, max);
+    }
+  }
+
+  return max;
+}
+
+/**
+ * Two pointer approach
+ * @param {number[]} height
+ * @return {number}
+ */
+function maxArea2(height) {
+  let max = 0;
+  let left = 0;
+  let right = height.length - 1;
+
+  while (left < right) {
+    const area = Math.min(height[left], height[right]) * (right - left);
+    max = Math.max(area, max);
+
+    if (height[left] == height[right]) {
+      left++;
+      right--;
+    } else if (height[left] < height[right]) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+
+  return max;
+}
+
+export { maxArea, maxArea2 };
---
+++ JS/tests/container_with_most_water.test.js
@@ -0,0 +1,37 @@
+import { maxArea, maxArea2 } from "../src/container_with_most_water.js";
+import assert from "node:assert";
+import { describe, it } from "node:test";
+
+function runTests(name, callback) {
+  describe(name, { concurrency: true, timeout: 1000 }, () => {
+    it("should return 24 for input [2, 7, 8, 3, 7, 6]", () => {
+      const height = [2, 7, 8, 3, 7, 6];
+      const expected = 24;
+
+      const actual = callback(height);
+
+      assert.strictEqual(actual, expected);
+    });
+
+    it("should return 49 for input [1,8,6,2,5,4,8,3,7]", () => {
+      const height = [1, 8, 6, 2, 5, 4, 8, 3, 7];
+      const expected = 49;
+
+      const actual = callback(height);
+
+      assert.strictEqual(actual, expected);
+    });
+
+    it("should return 1 for input [1, 1]", () => {
+      const height = [1, 1];
+      const expected = 1;
+
+      const actual = callback(height);
+
+      assert.strictEqual(actual, expected);
+    });
+  });
+}
+
+runTests("maxArea", maxArea);
+runTests("maxArea2", maxArea2);
