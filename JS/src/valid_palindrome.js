3c52d90ecce8b19e1f6233ec5e607a05de11e5b4
Vo Quang Chien <2giosangmitom@proton.me>
2025-02-08 11:14:05 +0700

    js: add valid_palindrome 2

--- JS/src/valid_palindrome.js
+++ JS/src/valid_palindrome.js
@@ -3,15 +3,42 @@
  * @return {boolean}
  */
 function isPalindrome(s) {
-	s = s.replace(/[^a-z]|\s/gi, "").toLowerCase();
+  s = s.replace(/[^a-z]|\s/gi, "").toLowerCase();
 
-	for (let i = 0; i < s.length; i++) {
-		if (s[i] !== s[s.length - i - 1]) {
-			return false;
-		}
-	}
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] !== s[s.length - i - 1]) {
+      return false;
+    }
+  }
 
-	return true;
+  return true;
 }
 
-export { isPalindrome };
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+function isPalindrome2(s) {
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    const pattern = /[a-z0-9]/i;
+    while (left < right && !pattern.test(s[left])) {
+      left++;
+    }
+    while (left < right && !pattern.test(s[right])) {
+      right--;
+    }
+
+    if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+
+  return true;
+}
+
+export { isPalindrome, isPalindrome2 };
--- JS/tests/valid_palindrome.test.js
+++ JS/tests/valid_palindrome.test.js
@@ -1,47 +1,52 @@
 import assert from "node:assert";
 import { describe, it } from "node:test";
-import { isPalindrome } from "../src/valid_palindrome.js";
+import { isPalindrome, isPalindrome2 } from "../src/valid_palindrome.js";
 
-describe("valid palindrome", { concurrency: true, timeout: 1000 }, () => {
-	it("should return true for input 'A man, a plan, a canal: Panama'", () => {
-		const s = "A man, a plan, a canal: Panama";
-		const expected = true;
-		const actual = isPalindrome(s);
-		assert.equal(actual, expected);
-	});
+function runTests(name, callback) {
+  describe(name, { concurrency: true, timeout: 1000 }, () => {
+    it("should return true for input 'A man, a plan, a canal: Panama'", () => {
+      const s = "A man, a plan, a canal: Panama";
+      const expected = true;
+      const actual = callback(s);
+      assert.equal(actual, expected);
+    });
 
-	it("should return true for input 'racecar'", () => {
-		const s = "racecar";
-		const expected = true;
-		const actual = isPalindrome(s);
-		assert.equal(actual, expected);
-	});
+    it("should return true for input 'racecar'", () => {
+      const s = "racecar";
+      const expected = true;
+      const actual = callback(s);
+      assert.equal(actual, expected);
+    });
 
-	it("should return false for input 'hello'", () => {
-		const s = "hello";
-		const expected = false;
-		const actual = isPalindrome(s);
-		assert.equal(actual, expected);
-	});
+    it("should return false for input 'hello'", () => {
+      const s = "hello";
+      const expected = false;
+      const actual = callback(s);
+      assert.equal(actual, expected);
+    });
 
-	it("should return true for input ' ' (empty string)", () => {
-		const s = " ";
-		const expected = true;
-		const actual = isPalindrome(s);
-		assert.equal(actual, expected);
-	});
+    it("should return true for input ' ' (empty string)", () => {
+      const s = " ";
+      const expected = true;
+      const actual = callback(s);
+      assert.equal(actual, expected);
+    });
 
-	it("should return true for input 'No lemon, no melon'", () => {
-		const s = "No lemon, no melon";
-		const expected = true;
-		const actual = isPalindrome(s);
-		assert.equal(actual, expected);
-	});
+    it("should return true for input 'No lemon, no melon'", () => {
+      const s = "No lemon, no melon";
+      const expected = true;
+      const actual = callback(s);
+      assert.equal(actual, expected);
+    });
 
-	it("should return false for input '123abc321'", () => {
-		const s = "123abc321";
-		const expected = false;
-		const actual = isPalindrome(s);
-		assert.equal(actual, expected);
-	});
-});
+    it("should return false for input '123abc321'", () => {
+      const s = "123abc321";
+      const expected = false;
+      const actual = callback(s);
+      assert.equal(actual, expected);
+    });
+  });
+}
+
+runTests("isPalindrome", isPalindrome);
+runTests("isPalindrome2", isPalindrome2);
