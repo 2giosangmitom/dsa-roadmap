440b405ed8d18acbaaf33e364c2b84370ca672c1
Vo Quang Chien <2giosangmitom@proton.me>
2025-02-15 09:56:52 +0700

    cc: add longest consecutive sequence test

---
+++ C++/src/longest_consecutive_sequence.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  int longestConsecutive(vector<int> &nums);
+};
---
+++ C++/tests/longest_consecutive_sequence_test.cc
@@ -0,0 +1,62 @@
+#include "../src/longest_consecutive_sequence.hpp"
+#include <gtest/gtest.h>
+#include <vector>
+
+class LongestConsecutiveSequenceTest : public testing::Test {
+protected:
+  Solution s;
+};
+
+TEST_F(LongestConsecutiveSequenceTest, case1) {
+  std::vector<int> nums = {100, 4, 200, 1, 3, 2};
+  int expected = 4;
+  EXPECT_EQ(s.longestConsecutive(nums), expected);
+}
+
+TEST_F(LongestConsecutiveSequenceTest, case2) {
+  std::vector<int> nums = {0, 3, 7, 2, 5, 8, 4, 6, 0, 1};
+  int expected = 9;
+  EXPECT_EQ(s.longestConsecutive(nums), expected);
+}
+
+TEST_F(LongestConsecutiveSequenceTest, emptyArray) {
+  std::vector<int> nums = {};
+  int expected = 0;
+  EXPECT_EQ(s.longestConsecutive(nums), expected);
+}
+
+TEST_F(LongestConsecutiveSequenceTest, singleElement) {
+  std::vector<int> nums = {42};
+  int expected = 1;
+  EXPECT_EQ(s.longestConsecutive(nums), expected);
+}
+
+TEST_F(LongestConsecutiveSequenceTest, allSameNumbers) {
+  std::vector<int> nums = {7, 7, 7, 7};
+  int expected = 1;
+  EXPECT_EQ(s.longestConsecutive(nums), expected);
+}
+
+TEST_F(LongestConsecutiveSequenceTest, duplicateNumbers) {
+  std::vector<int> nums = {1, 2, 2, 3, 4, 4, 5};
+  int expected = 5;
+  EXPECT_EQ(s.longestConsecutive(nums), expected);
+}
+
+TEST_F(LongestConsecutiveSequenceTest, negativeAndPositive) {
+  std::vector<int> nums = {-3, -2, -1, 0, 1, 2, 3, 4};
+  int expected = 8;
+  EXPECT_EQ(s.longestConsecutive(nums), expected);
+}
+
+TEST_F(LongestConsecutiveSequenceTest, longSequenceInMiddle) {
+  std::vector<int> nums = {10, 20, 30, 40, 1, 2, 3, 4, 5, 100, 200};
+  int expected = 5;
+  EXPECT_EQ(s.longestConsecutive(nums), expected);
+}
+
+TEST_F(LongestConsecutiveSequenceTest, largeNumbers) {
+  std::vector<int> nums = {INT_MIN, INT_MIN + 1, INT_MAX - 1, INT_MAX};
+  int expected = 2;
+  EXPECT_EQ(s.longestConsecutive(nums), expected);
+}
