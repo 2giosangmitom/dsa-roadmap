dcbc4f2f798ae8bf8794373ed41fdfe7c98b739b
Vo Quang Chien <2giosangmitom@proton.me>
2025-02-16 09:50:19 +0700

    cc: add 234

---
+++ C++/src/palindrome_linked_list.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "../lib/linked_list.hpp"
+
+class Solution {
+public:
+  ListNode *find_mid(ListNode *head);
+  ListNode *reverse(ListNode *head);
+  bool isPalindrome(ListNode *head);
+};
