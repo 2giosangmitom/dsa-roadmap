bce0bee1b72a2d7a1e1fbd7ae8cb7976294a4008
Vo Quang Chien <2giosangmitom@proton.me>
2025-02-27 14:11:41 +0700

    perf(cc): optimize 3

--- C++/src/longest_substr_without_repeat.cc
+++ C++/src/longest_substr_without_repeat.cc
@@ -1,21 +1,20 @@
 #include "longest_substr_without_repeat.hpp"
-#include <unordered_set>
+#include <unordered_map>
 
 int Solution::lengthOfLongestSubstring(string s) {
-  unordered_set<char> set;
-  int result = 0;
+  unordered_map<char, int> prev_indexes;
+  int max_len = 0;
   int left = 0, right = 0;
 
   while (size_t(right) < s.size()) {
-    while (set.contains(s[right])) {
-      set.erase(s[left]);
-      left++;
+    if (prev_indexes.contains(s[right]) && prev_indexes[s[right]] >= left) {
+      left = prev_indexes[s[right]] + 1;
     }
 
-    result = max(result, right - left + 1);
-    set.insert(s[right]);
+    max_len = max(max_len, right - left + 1);
+    prev_indexes[s[right]] = right;
     right++;
   }
 
-  return result;
+  return max_len;
 }
