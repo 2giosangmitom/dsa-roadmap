e8d452f0b2e541e0be543bdafbea1f65a6923e36
Vo Quang Chien <2giosangmitom@proton.me>
2025-03-02 14:53:12 +0700

    cc: add daily 2570

---
+++ C++/src/merge_two_2d_arrays_by_summing_values.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  vector<vector<int>> mergeArrays(vector<vector<int>> &nums1,
+                                  vector<vector<int>> &nums2);
+  vector<vector<int>> mergeArrays2(vector<vector<int>> &nums1,
+                                   vector<vector<int>> &nums2);
+};
