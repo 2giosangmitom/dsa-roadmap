1583ab913c7a3d6e89b6007cd1fe82763d15810f
Vo Quang Chien <2giosangmitom@proton.me>
2025-03-04 10:17:32 +0700

    cc: add 125

---
+++ C++/solutions_125/solutions_125_tests.cc
@@ -0,0 +1,25 @@
+#include <gtest/gtest.h>
+
+#include <tuple>
+
+#include "solutions_125.hpp"
+
+class Solutions_125_Test : public testing::TestWithParam<tuple<string, bool>> {
+ protected:
+  Solution solution;
+};
+
+TEST_P(Solutions_125_Test, ) {
+  auto [s, expected] = GetParam();
+
+  bool actual = solution.isPalindrome(s);
+
+  EXPECT_EQ(actual, expected);
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    , Solutions_125_Test,
+    testing::Values(make_tuple("A man, a plan, a canal: Panama", true),
+                    make_tuple("race a car", false), make_tuple(" ", true),
+                    make_tuple("a dog! a panic in a pagoda.", true),
+                    make_tuple("abc123", false), make_tuple("0P", false)));
