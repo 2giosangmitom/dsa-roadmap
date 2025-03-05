c0b29ab724f63092f47c24c4871a70d639e65843
Vo Quang Chien <2giosangmitom@proton.me>
2025-03-05 14:45:00 +0700

    cc: update 3

--- C++/solutions_3/solutions_3.hpp
+++ C++/solutions_3/solutions_3.hpp
@@ -2,24 +2,44 @@
 
 #include <string>
 #include <unordered_map>
+#include <unordered_set>
 using namespace std;
 
 class Solution {
  public:
   int lengthOfLongestSubstring(string s) {
-    unordered_map<char, size_t> prev_indexes;
+    // The last position of the character
+    unordered_map<char, size_t> last_seen;
 
     int result = 0;
     size_t left = 0;
     size_t right = 0;
 
     while (right < s.size()) {
-      if (prev_indexes.contains(s[right]) && prev_indexes[s[right]] >= left) {
-        left = prev_indexes[s[right]] + 1;
+      if (last_seen.contains(s[right]) && last_seen[s[right]] >= left) {
+        left = last_seen[s[right]] + 1;
       }
-      prev_indexes[s[right]] = right;
+      last_seen[s[right]] = right;
       result = max(result, int(right - left + 1));
       right++;
+    }
+
+    return result;
+  }
+
+  int lengthOfLongestSubstring2(string s) {
+    int left = 0;
+    int result = 0;
+    unordered_set<char> char_set;
+
+    for (size_t right = 0; right < s.length(); right++) {
+      while (char_set.contains(s[right])) {
+        char_set.erase(s[left]);
+        left++;
+      }
+
+      char_set.insert(s[right]);
+      result = max(result, int(right - left + 1));
     }
 
     return result;
--- C++/solutions_3/solutions_3_tests.cc
+++ C++/solutions_3/solutions_3_tests.cc
@@ -9,10 +9,18 @@ class Solutions_3_Test : public testing::TestWithParam<tuple<string, int>> {
   Solution solution;
 };
 
-TEST_P(Solutions_3_Test, ) {
+TEST_P(Solutions_3_Test, first) {
   auto [s, expected] = GetParam();
 
   int actual = solution.lengthOfLongestSubstring(s);
+
+  EXPECT_EQ(actual, expected);
+}
+
+TEST_P(Solutions_3_Test, second) {
+  auto [s, expected] = GetParam();
+
+  int actual = solution.lengthOfLongestSubstring2(s);
 
   EXPECT_EQ(actual, expected);
 }
