6095f04a5bf4569a6c0f77ca8f9a7fad7c277de2
Vo Quang Chien <voquangchien.dev@proton.me>
2025-03-30 22:54:53 +0700

    cc(528): remove unneeded vector

--- C++/solutions_528/solutions_528.hpp
+++ C++/solutions_528/solutions_528.hpp
@@ -7,15 +7,14 @@ using namespace std;
 class Solution {
 private:
   vector<int> prefix_sums;
-  vector<int> weights;
 
 public:
-  Solution(vector<int> &w) : weights{w} {
-    prefix_sums.resize(weights.size());
+  Solution(vector<int> &w) {
+    prefix_sums.resize(w.size());
 
-    prefix_sums[0] = weights[0];
-    for (size_t i = 1; i < weights.size(); i++) {
-      prefix_sums[i] = prefix_sums[i - 1] + weights[i];
+    prefix_sums[0] = w[0];
+    for (size_t i = 1; i < w.size(); i++) {
+      prefix_sums[i] = prefix_sums[i - 1] + w[i];
     }
   }
 
