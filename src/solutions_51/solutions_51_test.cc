b9324116de8b1595da64397019746ddb778b90f1
Vo Quang Chien <voquangchien.dev@proton.me>
2025-06-10 08:57:06 +0700

    51: add more tests

--- src/solutions_51/solutions_51_test.cc
+++ src/solutions_51/solutions_51_test.cc
@@ -20,10 +20,24 @@ TEST_P(Solutions_51_Test, ) {
 }
 
 INSTANTIATE_TEST_SUITE_P(
-    , Solutions_51_Test,
-    testing::Values(std::make_tuple(4,
-                                    std::vector<std::vector<std::string>>{
-                                        {".Q..", "...Q", "Q...", "..Q."},
-                                        {"..Q.", "Q...", "...Q", ".Q.."}}),
-                    std::make_tuple(1, std::vector<std::vector<std::string>>{
-                                           {"Q"}})));
+    SolveNQueensTests, Solutions_51_Test,
+    testing::Values(
+        std::make_tuple(4,
+                        std::vector<std::vector<std::string>>{
+                            {".Q..", "...Q", "Q...", "..Q."},
+                            {"..Q.", "Q...", "...Q", ".Q.."}}),
+        std::make_tuple(1, std::vector<std::vector<std::string>>{{"Q"}}),
+        std::make_tuple(2, std::vector<std::vector<std::string>>{}),
+        std::make_tuple(3, std::vector<std::vector<std::string>>{}),
+        std::make_tuple(5,
+                        std::vector<std::vector<std::string>>{
+                            {"Q....", "..Q..", "....Q", ".Q...", "...Q."},
+                            {"Q....", "...Q.", ".Q...", "....Q", "..Q.."},
+                            {".Q...", "...Q.", "Q....", "..Q..", "....Q"},
+                            {".Q...", "....Q", "..Q..", "Q....", "...Q."},
+                            {"..Q..", "Q....", "...Q.", ".Q...", "....Q"},
+                            {"..Q..", "....Q", ".Q...", "...Q.", "Q...."},
+                            {"...Q.", "Q....", "..Q..", "....Q", ".Q..."},
+                            {"...Q.", ".Q...", "....Q", "..Q..", "Q...."},
+                            {"....Q", ".Q...", "...Q.", "Q....", "..Q.."},
+                            {"....Q", "..Q..", "Q....", "...Q.", ".Q..."}})));
