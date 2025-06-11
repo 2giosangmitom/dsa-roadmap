879ab2aa589f36b8dfef6ac1ddd175279b89dfaa
Vo Quang Chien <voquangchien.dev@proton.me>
2025-06-11 14:29:20 +0700

    add 17

---
+++ src/solutions_17/solutions_17_test.cc
@@ -0,0 +1,28 @@
+#include "solutions_17.hpp"
+
+#include <gtest/gtest.h>
+
+#include <tuple>
+#include <vector>
+
+class Solutions_17_Test
+    : public testing::TestWithParam<tuple<string, vector<string>>> {
+ protected:
+  Solution solutions;
+};
+
+TEST_P(Solutions_17_Test, ) {
+  auto [digits, expected] = GetParam();
+
+  auto actual = solutions.letterCombinations(digits);
+
+  EXPECT_EQ(actual, expected);
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    , Solutions_17_Test,
+    testing::Values(make_tuple("23",
+                               vector<string>{"ad", "ae", "af", "bd", "be",
+                                              "bf", "cd", "ce", "cf"}),
+                    make_tuple("", vector<string>{}),
+                    make_tuple("2", vector<string>{"a", "b", "c"})));
