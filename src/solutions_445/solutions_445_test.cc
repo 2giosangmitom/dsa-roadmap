ef6ff91036114300742dc818979284718aae1d17
Vo Quang Chien <voquangchien.dev@proton.me>
2025-06-13 17:02:44 +0700

    fix memory leak in 445

--- src/solutions_445/solutions_445_test.cc
+++ src/solutions_445/solutions_445_test.cc
@@ -13,10 +13,19 @@ class Solutions_445_Test
 };
 
 TEST_P(Solutions_445_Test, ) {
+    auto get_tail = [](ListNode* head) {
+        auto tail = head;
+        while (tail->next) {
+            tail = tail->next;
+        }
+        return tail;
+    };
     auto [l1_vals, l2_vals, expected_vals] = GetParam();
     auto l1 = make_list(l1_vals);
     auto l2 = make_list(l2_vals);
     auto expected = make_list(expected_vals);
+    auto l1_tail = get_tail(l1);
+    auto l2_tail = get_tail(l2);
 
     auto actual = solutions.addTwoNumbers(l1, l2);
 
@@ -24,8 +33,8 @@ TEST_P(Solutions_445_Test, ) {
         EXPECT_EQ(expected, nullptr);
     } else {
         EXPECT_EQ(*actual, *expected);
-        delete_list({actual, expected, l1, l2});
     }
+    delete_list({actual, expected, l1_tail, l2_tail});
 }
 
 INSTANTIATE_TEST_SUITE_P(
